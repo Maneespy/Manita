44fcc9b548231411bf334ccf300a2cdc4966642e
Maneespy <124641883+Maneespy@users.noreply.github.com>
2023-02-07 18:56:47 -0500

    Add files via upload

---
+++ AssignmentDay2.java
@@ -0,0 +1,14 @@
+public class AssignmentDay2 {
+    public static void main(String[] args) {
+        for (int i = 4; i <= 30;  i +=2)
+             {
+                System.out.println("even number i :"+i);
+            }
+        }
+    }
+
+
+
+   /* Write a program to print all the EVEN numbers from 3 to 30
+
+    */
---
+++ AssignmentWeek2.java
@@ -0,0 +1,13 @@
+public class AssignmentWeek2 {
+    public static void main(String[] args)
+    {
+        {
+            for (int i = 3; i <= 19;  i +=2)
+            {
+                System.out.println("odd number i :"+i);
+            }
+            }
+        }
+    }
+
+
